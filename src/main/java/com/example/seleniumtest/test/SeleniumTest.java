1d13873a479acf3b04965e716d609fc67a769122
EJ <hanej93@naver.com>
2023-07-30 00:13:29 +0900

    뽐뿌 최신게시물 10개 크롤링

--- src/main/java/com/example/seleniumtest/test/SeleniumTest.java
+++ src/main/java/com/example/seleniumtest/test/SeleniumTest.java
@@ -19,22 +19,35 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class SeleniumTest {
 
 	public static void main(String[] args) {
-		Path path = Paths.get(System.getProperty("user.dir"), "src/main/resources/chromedriver.exe");
-		
-		// WebDriver 경로 설정
-		System.setProperty("webdriver.chrome.driver", path.toString());
-		
-		// WebDriver 옵션 설정
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--start-maximized"); // 전체화면으로 실행
-		options.addArguments("--disable-popup-blocking"); // 팝업 무시
-		options.addArguments("--disable-default-apps"); // 기본 브라우저 사용 안함
+		printPpomppuFreeLatestArticles();
+	}
 
-		// WebDriver 객체 생성
-		ChromeDriver driver = new ChromeDriver(options);
+	private static void printPpomppuFreeLatestArticles() {
+		ChromeDriver driver = getChromeDriver();
+
+		List<String> tabs = new ArrayList<>(driver.getWindowHandles());
+
+		// 첫번째 탭으로 전환
+		driver.switchTo().window(tabs.get(0));
+		driver.get("https://www.ppomppu.co.kr/zboard/zboard.php?id=freeboard");
+
+		Util.sleep(1000); // 웹페이지가 띄워지는 시간 대기
+
+		List<WebElement> elements = driver.findElements(
+			By.cssSelector("#revolution_main_table tbody tr:not(.list_notice) > td:nth-child(3) > a"));
+		System.out.println(elements.size());
+
+		for (WebElement element : elements) {
+			String title = element.getText().trim();
+			System.out.println(title);
+		}
+	}
+
+	private static void downloadUnsplashNatureImages() {
+		ChromeDriver driver = getChromeDriver();
 
 		// 빈 탭 생성
-		driver.executeScript("window.open('about:black', '_blank');");
+		// driver.executeScript("window.open('about:black', '_blank');");
 
 		List<String> tabs = new ArrayList<>(driver.getWindowHandles());
 
@@ -55,6 +68,23 @@ public class SeleniumTest {
 
 			System.out.println("src = " + src);
 		}
+	}
+
+	private static ChromeDriver getChromeDriver() {
+		Path path = Paths.get(System.getProperty("user.dir"), "src/main/resources/chromedriver.exe");
+
+		// WebDriver 경로 설정
+		System.setProperty("webdriver.chrome.driver", path.toString());
+
+		// WebDriver 옵션 설정
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--start-maximized"); // 전체화면으로 실행
+		options.addArguments("--disable-popup-blocking"); // 팝업 무시
+		options.addArguments("--disable-default-apps"); // 기본 브라우저 사용 안함
+
+		// WebDriver 객체 생성
+		ChromeDriver driver = new ChromeDriver(options);
+		return driver;
 	}
 
 	private static void makeDownloadsFoldersIfNotExists() {
